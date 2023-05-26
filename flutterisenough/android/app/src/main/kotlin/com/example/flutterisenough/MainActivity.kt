dc9d4f8ae47a5a67c8cbe4f4b37aca9599374edf
abdulrasheed98 <120790829+abdulrasheed98@users.noreply.github.com>
2023-05-26 10:57:24 +0500

    Add files via upload

---
+++ flutterisenough/android/app/src/main/kotlin/com/example/flutterisenough/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterisenough
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutterisenough/lib/enough_screens/auth/signup_screens/signuppage_1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignUpPageOne extends StatelessWidget {
+  const SignUpPageOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
